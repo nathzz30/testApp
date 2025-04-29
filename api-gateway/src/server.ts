444f55e06d6e4ed3bf2be967ef6612f6280a8c4d
Nathaly Ramirez Persia <nathalypersia@gmail.com>
2025-04-28 22:31:08 -0400

    Adding health check on api-gateway server

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
 import { errorHandler } from './middlewares/errorHandler';
@@ -12,6 +12,12 @@ app.use('/', routes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`API Gateway running on port ${PORT}`);
+});
+
+// Simple health check route
+app.get("/health", (req: Request, res: Response) => {
+  res.send("✅ Auth Service is healthy");
 });
